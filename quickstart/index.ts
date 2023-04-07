6a5a74fc79fee2c4e14501a2b45af5b06356c912
zach.perkins <zach.perkins@mindbodyonline.com>
2023-04-07 13:03:03 -0500

    Setting config correct

--- quickstart/index.ts
+++ quickstart/index.ts
@@ -1,14 +1,12 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as resources from "@pulumi/azure-native/resources";
 import * as storage from "@pulumi/azure-native/storage";
-import { devStorageAcct }  from './dev';
 
 // Create an Azure Resource Group
 const resourceGroup = new resources.ResourceGroup("resourceGroup");
 
 // Create an Azure resource (Storage Account)
 const storageAccount = new storage.StorageAccount("sa", {
-    ...devStorageAcct,
     resourceGroupName: resourceGroup.name,
     sku: {
         name: storage.SkuName.Standard_LRS,
